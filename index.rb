1b700d7e3a63830ef18a209e09faa89203c0cfd8
fstacker <brett@yutster.com>
2019-04-22 21:04:16 -0600

    finished first rspec

--- index.rb
+++ index.rb
@@ -1,10 +1,13 @@
 require 'sinatra'
 
-=begin
-puts "Tell me your birtday.  Use the format: MMDDYYYY\n*beep boop*"
-birthdate = gets
-=end
 
+#1. Get the user's birthday and store in a variable
+def get_birthday
+	puts "Tell me your birtday.  Use the format: MMDDYYYY\n*beep boop*"
+	birthdate = gets
+end
+
+#2. Take the user's response and calculate the birth path number
 def get_birth_path_num(birthdate)
 	number = birthdate[0].to_i + birthdate[1].to_i + birthdate[2].to_i + birthdate[3].to_i + birthdate[4].to_i + birthdate[5].to_i + birthdate[6].to_i + birthdate[7].to_i
 
@@ -13,49 +16,46 @@ def get_birth_path_num(birthdate)
 
 	if number > 9
 		number = number.to_s
+		
+		#3.  Assign the return value to a variable
 		number = number[0].to_i + number[1].to_i
 	end
 
 	return number
 end
 
-def get_message(birth_path_num)
-	case birth_path_num
+#4.  Create a method that returns a specific message that corresponds to the birth path number.
+def get_message(number)
+	case number
 	when 1
-		message = "Your numerology number is #{birth_path_num}. \nOne is the leader.  The number one indicates the ability to stand alone and is a strong vibration.  Ruled by the Sun."
+		@message = "One is the leader.  The number one indicates the ability to stand alone and is a strong vibration.  Ruled by the Sun."
 	when 2
-		message = "Your numerology number is #{birth_path_num}. \nTwo is the mediator and peace-lover.  The number two indicates the desire for harmony.  It is a gentle, considerate, and sensitive vibration.  Ruled by the Moon."
+		@message = "Two is the mediator and peace-lover.  The number two indicates the desire for harmony.  It is a gentle, considerate, and sensitive vibration.  Ruled by the Moon."
 	when 3
-		message = "Your numerology number is #{birth_path_num}. \nThree is a sociable, friendly, and outgoing vibration.  Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor."
+		@message = "Three is a sociable, friendly, and outgoing vibration.  Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor."
 	when 4
-		message = "Your numerology number is #{birth_path_num}. \nThis is the worker.  Practical, with a love for detail, Fours are trustworthy, hard-working, and helpful.  Ruled by Youranus."
+		@message = "This is the worker.  Practical, with a love for detail, Fours are trustworthy, hard-working, and helpful.  Ruled by Youranus."
 	when 5
-		message = "Your numerology number is #{birth_path_num}. \nThis is the freedom lover.  The number five is an intellectual vibration.  These are \"idea\" people with a love of variety and the ability to adapt to most situations.  Ruled by Mercury."
+		@message = "This is the freedom lover.  The number five is an intellectual vibration.  These are \"idea\" people with a love of variety and the ability to adapt to most situations.  Ruled by Mercury."
 	when 6
-		message = "Your numerology number is #{birth_path_num}. \nThis is the peace lover.  The number six is a loving, stable, and harmonious vibration.  Ruled by Venus."
+		@message = "This is the peace lover.  The number six is a loving, stable, and harmonious vibration.  Ruled by Venus."
 	when 7
-		message = "Your numerology number is #{birth_path_num}. \nThis is the deep thinker.  The number seven is a spiritual vibration.  These people are not very attached to material things, are introspective, and generally quiet.  Ruled by Neptune."
+		@message = "This is the deep thinker.  The number seven is a spiritual vibration.  These people are not very attached to material things, are introspective, and generally quiet.  Ruled by Neptune."
 	when 8
-		message = "Your numerology number is #{birth_path_num}. \nThis is the manager.  Number Eight is a strong, successful, and material vibration.  Ruled by Saturn."
+		@message = "This is the manager.  Number Eight is a strong, successful, and material vibration.  Ruled by Saturn."
 	when 9
-		message = "Your numerology number is #{birth_path_num}. \nThis is the teacher.  Number Nine is a tolerant, somewhat impractical, and sympathetic vibration.  Ruled by Mars."
+		@message = "This is the teacher.  Number Nine is a tolerant, somewhat impractical, and sympathetic vibration.  Ruled by Mars."
 	else
-		message = "Uh oh!  Your birth path number is not 1-9!"
+		@message = "Uh oh!  Your birth path number is not 1-9!"
 	end
 end
 
 
 
-get '/' do
-	"Hello World"
-end
-
-
 get '/:birthdate' do
 	birthdate = params[:birthdate]
 	birth_path_num = get_birth_path_num(birthdate)
-	message = get_message(birth_path_num)
-	"#{message}"
+	@message = get_message(birth_path_num) + "#{@message}"
 end
 
 
@@ -66,4 +66,3 @@ get '/:birthdate' do
   @message = "Your numerology number is #{birth_path_num}. " + get_message(birth_path_num)
   erb :index
 =end
-end
